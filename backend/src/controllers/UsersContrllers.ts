7d500d4e1ca50be29399ea3d467e3bbd848cde9c
Francisco5z <git@github.com:Francisco5z/Teste.git>
2020-07-11 13:00:39 -0300

    Criado o controller de usuário

---
+++ backend/src/controllers/UsersContrllers.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from 'express';
+
+class UserControllers {
+  async index(req: Request, res: Response) {}
+  async show(req: Request, res: Response) {}
+  async create(req: Request, res: Response) {}
+  async update(req: Request, res: Response) {}
+  async delete(req: Request, res: Response) {}
+}
